6318a3c612d8e1d87ad71abc6c3c791f905e2ec0
Elijah <64851720+ElijahRus250@users.noreply.github.com>
2021-06-06 19:59:41 -0400

    Update twitch.js

--- src/twitch.js
+++ src/twitch.js
@@ -46,7 +46,7 @@ Twitch.prototype.getUserId = function(username) {
         // make request
         this.makeRequest(url)
             .then(data => {
-                let json = JSON.parse(data);
+                let json = data;
                 if (json.users[0] == null || json.users[0]._id == null) {
                     return cosnole.error(`The user ${username} seems to not exist`);
                 }
